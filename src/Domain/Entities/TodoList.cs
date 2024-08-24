2d92112ee2d4c56fe85aca11b6286c3120ad3b09
eraybulut34 <abdullaheraybulut@hotmail.com>
2024-08-25 02:13:54 +0300

    structure

---
+++ src/Domain/Entities/TodoList.cs
@@ -0,0 +1,11 @@
+﻿using CleanArchitecture.Domain.Common;
+
+namespace CleanArchitecture.Domain.Entities;
+
+public class TodoList : BaseAuditableEntity
+{
+    public string? Title { get; set; }
+
+
+    public IList<TodoItem> Items { get; private set; } = new List<TodoItem>();
+}
